002dcd07d363390a026b8528a94c3fe17d57a7cf
Ben Gardiner <gardinbe@gmail.com>
2025-05-30 01:43:28 +0100

    updates

--- src/pages/Home.ts
+++ src/pages/Home.ts
@@ -1,7 +1,11 @@
 import { html } from 'benjs';
+import { Link } from 'benjs-router';
 
 const HomePage = () => {
-  return html`<h1>Home</h1>`;
+  return html`
+    <h1>Home</h1>
+    ${Link({ href: '/about', content: 'About' })}
+  `;
 };
 
 export default HomePage;
