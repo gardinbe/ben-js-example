eebfcb9e7e53fd1509e6c43761370bdaabad7990
Ben Gardiner <gardinbe@gmail.com>
2025-09-12 12:02:35 +0100

    updates

--- src/routes.ts
+++ src/routes.ts
@@ -3,19 +3,19 @@ import { def, type Route } from 'benjs-router';
 export const routes: Route[] = [
   {
     path: '',
-    component: def(import('~/pages/Home'))
+    component: def(() => import('~/pages/Home'))
   },
   {
     path: 'about',
-    component: def(import('~/pages/About'))
+    component: def(() => import('~/pages/About'))
   },
   {
     path: 'pokemon',
-    component: def(import('~/pages/PokemonList')),
+    component: def(() => import('~/pages/PokemonList')),
     children: [
       {
         path: '[slug]',
-        component: def(import('~/pages/Pokemon'))
+        component: def(() => import('~/pages/Pokemon'))
       }
     ]
   }
